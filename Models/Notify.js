cc634f30d32985ebe7003e3141947848b505c869
KAPIL SIKKA <kapilsikka@KAPILs-Air.Dlink>
2021-06-26 19:03:30 +0530

    Use mailgun

--- Models/Notify.js
+++ Models/Notify.js
@@ -2,14 +2,14 @@ const nodemailer=require("nodemailer");
 require("dotenv").config();
 const send=async(people, notificationashtml, subject)=>{
     const transport=nodemailer.createTransport({
-        service:"gmail",
+        service:"mailgun",
         auth:{
             user: process.env.EMAIL_ADDRESS,
             pass: process.env.EMAIL_PASSWORD
         }
     });
     const mail=await transport.sendMail({
-        name:"mail.google.com",
+        name:"mailgun.com",
         from: process.env.EMAIL_ADDRESS,
         to:people,
         subject,
